521e77093e384ffcb4a7472a684004533b24c679
Brandon Betts <brandoloura@yahoo.com>
2024-03-24 19:44:48 -0400

    initial commit

---
+++ destructuringExercise.js
@@ -0,0 +1,18 @@
+//8
+//1846
+// {yearNeptuneDiscovered: 1846,yearMarsDiscovered: 1659}
+//"Your name is Alejandro and you like purple"
+//"Your name is Melissa and you like green"
+// "Your name is undefined and you like green"
+// "Maya"
+// "Marisa"
+// "Chi"
+// Raindrop on roses
+// whiskers on kittens
+//["Bright copper kettles","warm woolen mittens","Brown paper packages tied up with strings"]
+//[10,30,20]
+const obj = {a:1,b:2}
+let {a,b} = obj
+[arr[0],arr[1]]=[arr[1],arr[0]]
+let raceResults =['Tom', 'Margaret', 'Allison', 'David', 'Pierre']
+let {first,second,third, ...rest} = raceResults
